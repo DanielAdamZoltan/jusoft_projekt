22966f6bc96e3213d9e805b24c8337791614727b
Dániel Ádám <daniel.adam995@gmail.com>
2020-08-25 21:06:27 +0200

    first_commit

---
+++ src/main/java/com/jusoft/jusoft_projekt/JusoftProjektApplication.java
@@ -0,0 +1,13 @@
+package com.jusoft.jusoft_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusoftProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JusoftProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jusoft/jusoft_projekt/JusoftProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jusoft.jusoft_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JusoftProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
